81a95c1ea351ba5003e2f4b46df3544f0410a1b7
Emma Dupont <emmadupont@Emmas-MacBook-Air-3.local>
2026-02-18 11:33:16 +0100

    cleaned up the code

--- results/reports/images.py
+++ results/reports/images.py
@@ -1,62 +1,61 @@
-import pandas as pd
+# all images
 
-#Load the metadata
-metadata = pd.read_csv("./data/group_data/metadata.csv")
-metadata
+def all_img(data:str):
 
-# print all images
+    # import libraries
+    import os
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import pandas as pd
 
-# import libraries
-import os
-import matplotlib.pyplot as plt
-import numpy as np
+    #Load the metadata
+    metadata = pd.read_csv(data)
 
-#count 10 images
-count = 0
+    count = 0
 
-#choose 10 random
+    while count < metadata.shape[0]:
 
-while count < 117:
+        # all ID's and paths
+        pa_index = count
+        le_id = str(metadata.at[pa_index , "lesion_id"])
+        im_id = str(metadata.at[pa_index , "img_id"])
+        dir_path = './data/group_data/metadata.csv'
+        img_dir = "./data/group_data/imgs/"
+        mask_dir = "./data/group_data/masks/"
 
-    # all ID's and paths
-    pa_index = count
-    le_id = str(metadata.at[pa_index , "lesion_id"])
-    im_id = str(metadata.at[pa_index , "img_id"])
-    dir_path = './data/group_data/metadata.csv'
-    img_dir = "./data/group_data/imgs/"
-    mask_dir = "./data/group_data/masks/"
+        #making mask id
+        img_to_mask = metadata.at[pa_index , "img_id"]
+        split_mask = img_to_mask.split(".")
+        mask_id = split_mask[0] + "_mask"
 
-    #making mask id
-    img_to_mask = metadata.at[pa_index , "img_id"]
-    split_mask = img_to_mask.split(".")
-    mask_id = split_mask[0] + "_mask"
+        # chosen patients information
 
-    # chosen patients information
+        # path for image
+        im_filename = f"{im_id}"
+        im_path = os.path.join(img_dir, im_filename)
 
-    # path for image
-    im_filename = f"{im_id}"
-    im_path = os.path.join(img_dir, im_filename)
+        # path for mask
+        mask_filename = f"{mask_id}.png"
+        mask_path = os.path.join(mask_dir, mask_filename)
 
-    # path for mask
-    mask_filename = f"{mask_id}.png"
-    mask_path = os.path.join(mask_dir, mask_filename)
+        # making subplot
+        fig = plt.figure()
+        im = plt.imread(im_path)
+        ax1 = fig.add_subplot(2,2,1)
+        ax1.imshow(im)
+        mask = plt.imread(mask_path)
+        ax2 = fig.add_subplot(2,2,2)
+        ax2.imshow(mask)
 
-    # making subplot
-    fig = plt.figure()
-    im = plt.imread(im_path)
-    ax1 = fig.add_subplot(2,2,1)
-    ax1.imshow(im)
-    mask = plt.imread(mask_path)
-    ax2 = fig.add_subplot(2,2,2)
-    ax2.imshow(mask)
+        # removing axes
+        ax1.set_axis_off()
+        ax2.set_axis_off()
 
-    # removing axes
-    ax1.set_axis_off()
-    ax2.set_axis_off()
+        # adding titles
+        ax1.title.set_text('Image')
+        ax2.title.set_text('Mask')
+        fig.suptitle(f"Patient: {metadata.at[pa_index , "img_id"]}, diagnostic: {metadata.at[pa_index , "diagnostic"]}")
 
-    # adding titles
-    ax1.title.set_text('Image')
-    ax2.title.set_text('Mask')
-    fig.suptitle(f"Patient: {metadata.at[pa_index , "img_id"]}, diagnostic: {metadata.at[pa_index , "diagnostic"]}")
+        count += 1
 
-    count += 1
+all_img("./data/group_data/metadata.csv")
